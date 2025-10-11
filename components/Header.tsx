140911605cb0883c8a2bf2a4c6df28cb3e7796da
Rabii Bouhestine <rabiibouhestine@outlook.com>
2025-10-11 11:14:08 +0100

    cleanup

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex justify-between max-w-5xl mx-auto p-6">
-      <Link href="/">Game Lists</Link>
+      <Link href="/">GameLists</Link>
       <nav className="flex gap-3">
         <Link href="/games">Games</Link>
         <Link href="/lists">Lists</Link>
