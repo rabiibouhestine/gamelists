5cdf6d9b0a5d0582ee9edd13e6bea2c07367e3c0
Rabii Bouhestine <rabiibouhestine@outlook.com>
2025-10-13 14:55:43 +0100

    add settings page

--- app/users/[username]/settings/page.tsx
+++ app/users/[username]/settings/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 
-export default async function ProfilePage() {
+export default async function ProfileSettingsPage() {
   const supabase = await createClient();
 
   const { data, error } = await supabase.auth.getUser();
