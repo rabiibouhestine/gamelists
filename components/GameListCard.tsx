422fc7419d268a754fb8ffafa437b41454c85a0f
Rabii Bouhestine <rabiibouhestine@outlook.com>
2025-10-14 15:46:24 +0100

    update game list card style

--- components/GameListCard.tsx
+++ components/GameListCard.tsx
@@ -8,11 +8,10 @@ type GameListProps = {
 export default function GameListCard({ gamelist }: GameListProps) {
   return (
     <div className="mt-6">
-      <Link href={`/lists/${gamelist.list_id}`}>
+      <Link href={`/lists/${gamelist.list_id}`} className="hover:underline">
         <h3 className="font-bold text-2xl">{gamelist.title}</h3>
       </Link>
-      <p className="line-clamp-2">{gamelist.description}</p>
-      <div className="flex items-center gap-2 my-3">
+      <div className="flex items-center gap-2">
         <div className="flex items-center gap-1">
           <Image
             src={gamelist.creator_profile_img}
@@ -23,7 +22,7 @@ export default function GameListCard({ gamelist }: GameListProps) {
           />
           <Link
             href={`/users/${gamelist.creator_username}`}
-            className="font-bold"
+            className="font-bold hover:underline"
           >
             {gamelist.creator_username}
           </Link>
@@ -34,7 +33,7 @@ export default function GameListCard({ gamelist }: GameListProps) {
       </div>
       <Link
         href={`/lists/${gamelist.list_id}`}
-        className="w-full bg-card rounded-md mt-3 flex"
+        className="w-full bg-card rounded-md mt-3 overflow-hidden grid grid-cols-22 hover:ring hover:ring-accent-foreground"
       >
         {gamelist.games.map((game, index) => (
           <Image
@@ -43,8 +42,8 @@ export default function GameListCard({ gamelist }: GameListProps) {
             alt={game.name}
             width={246}
             height={352}
-            className="object-cover rounded-md -mr-3 shadow-[2px_0_7px_#000]"
-            style={{ zIndex: 8 - index }}
+            className="h-full object-cover rounded-md shadow-[2px_0_7px_#000] col-span-4 row-start-1"
+            style={{ zIndex: 8 - index, gridColumnStart: index * 3 + 1 }}
           />
         ))}
       </Link>
