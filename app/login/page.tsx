161bbbf7b867c468e22bed79a6abb3d24f04e448
Rabii Bouhestine <rabiibouhestine@outlook.com>
2025-10-20 15:49:47 +0100

    implement login form validation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,69 +1,9 @@
-import { login } from "@/lib/actions/login";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import LoginForm from "@/components/LoginForm";
 
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center mt-20">
-      <Card className="w-md">
-        <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-          <CardAction>
-            <Button asChild variant="link">
-              <Link href="/signup">Sign Up</Link>
-            </Button>
-          </CardAction>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6 mb-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" name="password" type="password" required />
-              </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <Button formAction={login} className="w-full">
-                Login
-              </Button>
-              <Button variant="outline" className="w-full">
-                Login with Google
-              </Button>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
+      <LoginForm />
     </div>
   );
 }
--- lib/actions/login.ts
+++ lib/actions/login.ts
@@ -1,23 +1,41 @@
 "use server";
 
+import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
+import { AuthSchema } from "@/lib/schemas";
 
-export async function login(formData: FormData) {
+export async function login(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  initialState: any,
+  formData: FormData
+) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
+  const rawData = {
+    email: formData.get("email"),
+    password: formData.get("password"),
   };
 
-  const { error } = await supabase.auth.signInWithPassword(data);
+  // Validate fields
+  const parseResult = AuthSchema.safeParse(rawData);
+  if (!parseResult.success) {
+    const treeErrors = z.treeifyError(parseResult.error);
+    return { validationErrors: treeErrors };
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(parseResult.data);
 
   if (error) {
-    redirect("/error");
+    console.error("Failed to login:", error);
+    return {
+      validationErrors: {
+        errors:
+          error.status === 400 ? "Invalid Credentials" : "Failed to login",
+        properties: null,
+      },
+    };
   }
 
   revalidatePath("/", "layout");
