b4302e7295cbbc9291a4036aee3009ceabe54f31
Rabii Bouhestine <rabiibouhestine@outlook.com>
2025-10-21 14:07:13 +0100

    add responsiveness

--- components/GameListCard.tsx
+++ components/GameListCard.tsx
@@ -7,6 +7,24 @@ type GameListProps = {
 };
 
 export default function GameListCard({ gamelist }: GameListProps) {
+  const responsiveConfigs = [
+    {
+      breakpoint: "block sm:hidden", // mobile
+      gridCols: "grid-cols-13",
+      slice: 4,
+    },
+    {
+      breakpoint: "hidden sm:block md:hidden", // tablet
+      gridCols: "grid-cols-19",
+      slice: 6,
+    },
+    {
+      breakpoint: "hidden md:block", // desktop+
+      gridCols: "grid-cols-25",
+      slice: gamelist.games.length, // show all
+    },
+  ];
+
   return (
     <div className="mt-6">
       <Link href={`/lists/${gamelist.list_id}`} className="hover:underline">
@@ -36,22 +54,26 @@ export default function GameListCard({ gamelist }: GameListProps) {
           {gamelist.nb_comments} <MessageCircle size={18} />
         </span>
       </div>
-      <Link
-        href={`/lists/${gamelist.list_id}`}
-        className="w-full bg-card rounded-sm sm:rounded-md mt-3 overflow-hidden grid grid-cols-25 hover:ring hover:ring-accent-foreground"
-      >
-        {gamelist.games.map((game, index) => (
-          <Image
-            key={game.id}
-            src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${game.img}.jpg`}
-            alt={game.name}
-            width={246}
-            height={352}
-            className="max-h-50 h-full object-cover rounded-sm sm:rounded-md shadow-[2px_0_7px_#000] col-span-4 row-start-1"
-            style={{ zIndex: 8 - index, gridColumnStart: index * 3 + 1 }}
-          />
-        ))}
-      </Link>
+      {responsiveConfigs.map(({ breakpoint, gridCols, slice }, i) => (
+        <div key={i} className={breakpoint}>
+          <Link
+            href={`/lists/${gamelist.list_id}`}
+            className={`w-full bg-card rounded-sm sm:rounded-md mt-3 overflow-hidden grid ${gridCols} hover:ring hover:ring-accent-foreground`}
+          >
+            {gamelist.games.slice(0, slice).map((game, index) => (
+              <Image
+                key={game.id}
+                src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${game.img}.jpg`}
+                alt={game.name}
+                width={246}
+                height={352}
+                className="max-h-50 h-full object-cover rounded-sm sm:rounded-md shadow-[2px_0_7px_#000] col-span-4 row-start-1"
+                style={{ zIndex: 8 - index, gridColumnStart: index * 3 + 1 }}
+              />
+            ))}
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
