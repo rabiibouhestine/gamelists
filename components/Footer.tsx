85404af5ab8fdb0418606c5e2164dc91c91b349e
Rabii Bouhestine <rabiibouhestine@outlook.com>
2025-12-09 10:26:57 +0100

    update links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,11 +4,7 @@ export default function Footer() {
   return (
     <footer className="flex items-center justify-center flex-wrap gap-2 bg-card py-8">
       © {year} Game Lists, by
-      <a
-        className="hover:underline"
-        href="https://rabiibouhestine.com"
-        target="_blank"
-      >
+      <a className="hover:underline" href="https://rabii.dev" target="_blank">
         Rabii Bouhestine
       </a>
     </footer>
